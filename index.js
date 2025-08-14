86ba97dcbb358a7836a0a1384cf3096a5a620a87
Ranger_11 <147799870+AMI11REZA@users.noreply.github.com>
2025-08-14 15:47:20 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,87 +1,45 @@
 const mc = require('minecraft-protocol');
 
-const SERVER_HOST = 'Enter Server IP Here';
-const SERVER_PORT = Enter Port Here;
-const MC_VERSION = '1.21.1';    //Do Not Change The Version, Install Via Version Or Via Backward Depending On You'r Aternos Server Version.
-const USERNAME = 'AFKBot_ByRanger';
-
-let isAlive = false;
-let pos = { x: 0, y: 0, z: 0 };
-let yaw = 0;
-let pitch = 0;
-let rotatingInterval = null;
-
-function startBot() {
-  const client = mc.createClient({
-    host: SERVER_HOST,
-    port: SERVER_PORT,
-    username: USERNAME,
-    version: MC_VERSION,
-    keepAlive: true,
+function connectBot() {
+  let client = mc.createClient({
+    host: 'Enter Server IP Here',
+    port: Enter Port Here,
+    username: 'AFKBot_ByRanger',
+    version: '1.21.1'    //Do Not Change The Version, Install Via Version Or Via Backward Depending On You'r Aternos Server Version.
   });
 
-  function startRotation() {
-    if (rotatingInterval) clearInterval(rotatingInterval);
-    rotatingInterval = setInterval(() => {
-      yaw = (yaw + 45) % 360;
-      client.write('position_look', {
-        x: pos.x,
-        y: pos.y,
-        z: pos.z,
-        yaw,
-        pitch,
-        onGround: true,
-      });
-    }, 500);
-  }
+  let pos = { x: 0, y: 0, z: 0 }, yaw = 0, timer = null;
 
-  function safeMemoryCleanUp() {
-    const usedMB = process.memoryUsage().heapUsed / 1024 / 1024;
-    if (usedMB > 30) {
-      pos = { x: pos.x, y: pos.y, z: pos.z };
-      yaw = yaw % 360;
-      pitch = pitch % 360;
+  client.once('position', p => {
+    pos = { x: p.x, y: p.y, z: p.z };
+    timer = setInterval(() => {
+      try {
+        client.write('position_look', { x: pos.x, y: pos.y, z: pos.z, yaw: (yaw = (yaw + 45) % 360), pitch: 0, onGround: true });
+      } catch {}
+    }, 1000);
+  });
 
-      // Manual garbage collection
-      if (global.gc) {
-        global.gc();
-      }
+  client.on('update_health', h => {
+    if (h.health <= 0) client.write('client_command', { actionId: 0 });
+  });
+
+  const cleanup = () => {
+    if (timer) clearInterval(timer);
+    if (client) {
+      client.removeAllListeners();
+      if (client.socket) client.socket.destroy();
     }
-  }
+    pos = null;
+    yaw = null;
+    timer = null;
+    client = null;
+    global.gc?.();
+  };
 
-  client.on('position', (data) => {
-    pos = { x: data.x, y: data.y, z: data.z };
-    if (!isAlive) {
-      isAlive = true;
-    }
-    startRotation();
-  });
-
-  client.on('update_health', (data) => {
-    if (data.health <= 0) {
-      isAlive = false;
-      setTimeout(() => {
-        client.write('client_command', { actionId: 0 });
-      }, 500);
-    } else if (!isAlive && data.health > 0) {
-      isAlive = true;
-      startRotation();
-    }
-  });
-
-  client.on('end', () => {
-    console.log('[x] Disconnected. Reconnecting in 3 seconds...');
-    clearInterval(rotatingInterval);
-    rotatingInterval = null;
-    setTimeout(startBot, 3000);
-  });
-
-  client.on('error', (err) => {
-    console.log(`[!] Error: ${err.message}`);
-    setTimeout(startBot, 3000);
-  });
-
-  setInterval(safeMemoryCleanUp, 10000);
+  client.on('end', () => { cleanup(); setTimeout(connectBot, 100); });
+  client.on('error', () => { cleanup(); setTimeout(connectBot, 100); });
 }
 
-startBot();
+setInterval(() => { try { global.gc(); } catch {} }, 60000);
+
+connectBot();
