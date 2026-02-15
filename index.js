d7bf6454590bef38b6616417fb55e95a71af52f6
Ranger_11 <147799870+AMI11REZA@users.noreply.github.com>
2026-02-15 14:43:55 +0330

    Update index.js
    added custom username support

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const mc = require('minecraft-protocol');
 
 const HOST = process.env.HOST;
 const PORT = Number.parseInt(process.env.PORT, 10);
-const USERNAME = process.env.USERNAME;
+const USERNAME = process.env.USERNAME || 'AFKBot_ByRanger';
 const VERSION = '1.8.9';
 
 let client = null;
@@ -64,6 +64,7 @@ function connect() {
 
 process.on('uncaughtException', () => { cleanup(); setTimeout(connect, 100); });
 connect();
+
 
 
 
